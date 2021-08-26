5c28f72cbfd933dee15b1ae1d9940ca58231e16e
DevLangat <kevokipa4@gmail.com>
2021-08-26 13:03:18 +0300

    geolocation

--- lib/authenticate/usertype.dart
+++ lib/authenticate/usertype.dart
@@ -1,9 +1,10 @@
-
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
 import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
 class UserType extends StatefulWidget {
   const UserType({Key? key}) : super(key: key);
 
@@ -12,22 +13,52 @@ class UserType extends StatefulWidget {
 }
 
 class _UserTypeState extends State<UserType> {
-
-
   String _genderRadioBtnVal="patient";
-  Position? _currentPosition;
-  _getCurrentLocation() {
-    Geolocator
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best, forceAndroidLocationManager: true)
-        .then((Position position) {
-      setState(() {
-        _currentPosition = position;
-      });
-    }).catchError((e) {
-      print(e);
-    });
-  }
+  String currentAddress = 'My Address';
+  Position? currentposition;
 
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      Fluttertoast.showToast(msg: 'Please enable Your Location Service');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        Fluttertoast.showToast(msg: 'Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      Fluttertoast.showToast(
+          msg:
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    try {
+      List<Placemark> placemarks =
+      await placemarkFromCoordinates(position.latitude, position.longitude);
+
+      Placemark place = placemarks[0];
+
+      setState(() {
+        currentposition = position;
+        currentAddress =
+        "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
+    } catch (e) {
+      print(e);
+    }
+    return position;
+  }
 
 
   void _handleGenderChange(String ? value) {
@@ -76,56 +107,59 @@ class _UserTypeState extends State<UserType> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text("Select Your Role : ",style: TextStyle(color: Colors.black,fontSize: 25),),
-                                  ListTile(
-                                    title: Text("Patient",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
-                                    leading: Radio(
-                                      value: "patient",
-                                      groupValue: _genderRadioBtnVal,
-                                      onChanged: _handleGenderChange,
-                                      activeColor: Colors.green,
-                                    ),
-                                  ),
-                                  ListTile(
-                                    title: Text("Doctor",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
-                                    leading: Radio(
-                                      value: "doctor",
-                                      groupValue: _genderRadioBtnVal,
-                                      onChanged: _handleGenderChange,
-                                      activeColor: Colors.green,
-                                    ),
-                                  ),
-                                  ListTile(
-                                    title: Text("Pharmacy",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
-                                    leading: Radio(
-                                      value: "pharmacy",
-                                      groupValue: _genderRadioBtnVal,
-                                      onChanged: _handleGenderChange,
-                                      activeColor: Colors.green,
-                                    ),
-                                  ),
-                                  ListTile(
-                                    title: Text("Lab",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
-                                    leading: Radio(
-                                      value: "lab",
-                                      groupValue: _genderRadioBtnVal,
-                                      onChanged: _handleGenderChange,
-                                      activeColor: Colors.green,
-                                    ),
-                                  ),
-                                ],
-
-                         ),
-                            Padding(padding: const EdgeInsets.all(10.0),
-                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                if(_currentPosition != null) Text(
-                                    "LAT: ${_currentPosition!.latitude}, LNG: ${_currentPosition!.longitude}"
+                                Text("Select Your Role : ",style: TextStyle(color: Colors.black,fontSize: 25),),
+                                ListTile(
+                                  title: Text("Patient",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+                                  leading: Radio(
+                                    value: "patient",
+                                    groupValue: _genderRadioBtnVal,
+                                    onChanged: _handleGenderChange,
+                                    activeColor: Colors.green,
+                                  ),
+                                ),
+                                ListTile(
+                                  title: Text("Doctor",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+                                  leading: Radio(
+                                    value: "doctor",
+                                    groupValue: _genderRadioBtnVal,
+                                    onChanged: _handleGenderChange,
+                                    activeColor: Colors.green,
+                                  ),
+                                ),
+                                ListTile(
+                                  title: Text("Pharmacy",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+                                  leading: Radio(
+                                    value: "pharmacy",
+                                    groupValue: _genderRadioBtnVal,
+                                    onChanged: _handleGenderChange,
+                                    activeColor: Colors.green,
+                                  ),
+                                ),
+                                ListTile(
+                                  title: Text("Lab",style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo),),
+                                  leading: Radio(
+                                    value: "lab",
+                                    groupValue: _genderRadioBtnVal,
+                                    onChanged: _handleGenderChange,
+                                    activeColor: Colors.green,
+                                  ),
                                 ),
                               ],
+
                             ),
+                            Padding(padding: const EdgeInsets.all(10.0),
+                              child: Column(
+                                children: [
+                                  if(currentAddress != "") Text(
+                                      currentAddress,
+                                      style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo) ),
+                                  if(currentposition !=null) Text(
+                                      'Longitute: '+currentposition!.longitude.toString()+', Latitude: '+currentposition!.latitude.toString(),
+                                      style: TextStyle(fontWeight:FontWeight.bold,color:Colors.indigo) ),
+                                ],
+                              ),
                             ),
 
                             /////////////// SignUp Button ////////////
@@ -136,7 +170,7 @@ class _UserTypeState extends State<UserType> {
                                     padding: EdgeInsets.only(
                                         top: 8, bottom: 8, left: 10, right: 10),
                                     child: Text(
-                                       'Complete Registration',
+                                      'Complete Registration',
                                       textDirection: TextDirection.ltr,
                                       style: TextStyle(
                                         color: Colors.white,
@@ -152,10 +186,10 @@ class _UserTypeState extends State<UserType> {
                                       borderRadius:
                                       new BorderRadius.circular(20.0)),
                                   onPressed: (){
-                                    _getCurrentLocation();
+                                    _determinePosition();
                                     Fluttertoast.showToast(msg: _genderRadioBtnVal,toastLength: Toast.LENGTH_SHORT);
                                   }
-                            ),
+                              ),
                             )
                           ],
                         ),
